42d5f2a7671d06cadcad4656de5200de25af618b
Jason <sanchezjason@me.com>
2024-06-13 16:42:00 -0600

    mapkit/swiftui/search_map- isSearching, mapItems

--- CloseToMe/CloseToMe/CloseToMeView.swift
+++ CloseToMe/CloseToMe/CloseToMeView.swift
@@ -14,10 +14,28 @@ struct CloseToMeView: View {
     @State private var selectedDetent: PresentationDetent = .fraction(0.15)
     @State private var locationManager = LocationManager.shared
     @State private var position: MapCameraPosition = .userLocation(fallback: .automatic)
+    @State private var isSearching: Bool = false
+    @State private var mapItems: [MKMapItem] = []
+    
+    private func search() async {
+        do {
+            mapItems = try await performSearch(searchTerm: query,
+                                               visibleRegion: locationManager.region)
+            print(mapItems)
+            isSearching = false
+        } catch {
+            mapItems = []
+            print(error.localizedDescription)
+            isSearching = false
+        }
+    }
     
     var body: some View {
         ZStack {
             Map(position: $position) {
+                ForEach(mapItems, id: \.self) { mapItem in
+                    Marker(item: mapItem)
+                }
                 UserAnnotation()
             }
             .onChange(of: locationManager.region, {
@@ -29,7 +47,7 @@ struct CloseToMeView: View {
                         .textFieldStyle(.roundedBorder)
                         .padding()
                         .onSubmit {
-                            // code fired when you tap return in TextField
+                           isSearching = true
                         }
                     Spacer()
                 }
@@ -39,6 +57,11 @@ struct CloseToMeView: View {
                 .presentationBackgroundInteraction(.enabled(upThrough: .medium))
             })
         }
+        .task(id: isSearching, {
+            if isSearching {
+                await search()
+            }
+        })
     }
 }
 
